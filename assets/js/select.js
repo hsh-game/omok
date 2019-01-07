56f2bf1e9d003144b6205f8cd18177e959f499aa
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2019-01-08 00:01:58 +0900

    v0.5-Beta : [Initial commit]
    레포지티가 이동되었습니다. 이전 버전의 기록은 아래의 링크를 통해 확인하실 수 있습니다.
    
    https://github.com/Seed-Maker/ai-omok

---
+++ assets/js/select.js
@@ -0,0 +1,146 @@
+const user = {
+  color: EMPTY,
+  focus: {
+    coord: [7, 7],
+    set() {
+      //user.focus.set
+      const X = 0, Y = 1;
+      let board = game.getCanvas(),
+          ctx = board.ctx,
+          banedPosition = game.getBanedPosition(user.color),
+          x = board.padding + board.blockWidth * user.focus.coord[0],
+          y = board.padding + board.blockWidth * user.focus.coord[1],
+          r = board.width / 35;
+
+      game.stone.update();
+      ctx.lineWidth = 7;
+      ctx.strokeStyle = '#fa6060';
+      ctx.beginPath();
+      ctx.arc(x, y, r, 0, 2 * Math.PI);
+      ctx.moveTo(x - board.blockWidth/1.5, y);
+      ctx.lineTo(x + board.blockWidth/1.5, y);
+      ctx.moveTo(x, y - board.blockWidth/1.5);
+      ctx.lineTo(x, y + board.blockWidth/1.5);
+      ctx.stroke();
+
+      if (!game.checkWin())
+      for (i = 0; i < banedPosition.length; i++) {
+        const BX = board.padding + board.blockWidth * banedPosition[i][X],
+              BY = board.padding + board.blockWidth * banedPosition[i][Y],
+              LEN = board.blockWidth/3;
+        ctx.strokeStyle = '#7c1b1b';
+        ctx.lineWidth = 15;
+        ctx.beginPath();
+        ctx.moveTo(BX - LEN, BY - LEN);
+        ctx.lineTo(BX + LEN, BY + LEN);
+        ctx.moveTo(BX - LEN, BY + LEN);
+        ctx.lineTo(BX + LEN, BY - LEN);
+        ctx.stroke();
+      }
+    }
+  },
+
+  set() {
+    //user.set
+    const AI_COLOR = (user.color == BLACK)? WHITE : BLACK,
+          X = 0, Y = 1;
+
+    if (!user.color || game.stone.isStone(...user.focus.coord)) return;
+
+    const IS_BANED = game.getBanedPosition(user.color)
+                         .map(JSON.stringify)
+                         .includes(
+                           JSON.stringify(user.focus.coord)
+                         );
+
+    if (IS_BANED)
+      return alert("그곳에는 돌을 놓을 수 없습니다. 룰을 확인해주세요.");
+
+    game.stone.set(user.color, ...user.focus.coord);
+    game.stone.set(AI_COLOR, ...AI(AI_COLOR, game.stone.list));
+    user.focus.set();
+
+    let winner = game.checkWin();
+    if (winner) {
+      setTimeout(() => {
+        alert("당신의 " + ((winner == user.color)? "승리":"패배") + "입니다.");
+      }, 100);
+    }
+  }
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  user.focus.set();
+
+  $('#set-btn').addEventListener('click', user.set, false);
+  $('#version').innerHTML = game.version;
+
+  function initGame(userColor) {
+    user.color = userColor;
+    $('#game-explain').style.display = 'block';
+    $('#stone-color-select').style.display = 'none';
+    $('#control-keys').style.display = 'grid';
+  }
+
+  $('#select-white').addEventListener('click', () => {
+    initGame(WHITE);
+    game.stone.set(BLACK, ...AI(BLACK, game.stone.list));
+    user.focus.set();
+  }, false);
+
+  $('#select-black').addEventListener('click', () => {
+    initGame(BLACK);
+  }, false);
+
+  function checkFocus() {
+    if (user.focus.coord[1] < 0) user.focus.coord[1] = 0;
+    if (user.focus.coord[0] < 0) user.focus.coord[0] = 0;
+    if (user.focus.coord[1] > 14) user.focus.coord[1] = 14;
+    if (user.focus.coord[0] > 14) user.focus.coord[0] = 14;
+    user.focus.set();
+  }
+
+  $('#move-up-btn').addEventListener('click', () => {
+    user.focus.coord[1]--;
+    checkFocus();
+  }, false);
+
+  $('#move-down-btn').addEventListener('click', () => {
+    user.focus.coord[1]++;
+    checkFocus();
+  }, false);
+
+  $('#move-left-btn').addEventListener('click', () => {
+    user.focus.coord[0]--;
+    checkFocus();
+  }, false);
+
+  $('#move-right-btn').addEventListener('click', () => {
+    user.focus.coord[0]++;
+    checkFocus();
+  }, false);
+
+  window.addEventListener('keydown', e => {
+    switch (e.key) {
+      case 'ArrowDown':
+        user.focus.coord[1]++;
+        break;
+      case 'ArrowUp':
+        user.focus.coord[1]--;
+        break;
+      case 'ArrowLeft':
+        user.focus.coord[0]--;
+        break;
+      case 'ArrowRight':
+        user.focus.coord[0]++;
+        break;
+      case ' ':
+      case 'Enter':
+        user.set();
+        break;
+      default:
+        return;
+    }
+    checkFocus();
+  }, false);
+}, false);
